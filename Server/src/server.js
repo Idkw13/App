55122e3c004e55f30e53323cfb0a7bb70933ef4a
idkw13 <nazarii.maiba@indealpro.com>
2020-03-20 14:02:06 +0200

    added headers

--- Server/src/server.js
+++ Server/src/server.js
@@ -8,6 +8,24 @@ dotenv.config()
 
 const app = express()
 
+app.use((req, res, next) => {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8082');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, auth-token');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
 // parse requests of content-type - application/json
 app.use(bodyParser.json())
 // parse requests of content-type - application/x-www-form-urlencoded
